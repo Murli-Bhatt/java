18d2ef53d840e99487254432e3317b1c1a36481d
Murli-Bhatt <murlibhatt52@gmail.com>
2025-10-12 23:48:45 +0530

    Update Dyna1d.java

--- Dyna1d.java
+++ Dyna1d.java
@@ -2,14 +2,14 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 public class Dyna1d {
-    public static int fibonacci(int n, int[] mp) {
+    public static int fibonacci(int n, int[] dp) {
         if (n <= 1) {
             return n;
         }
-        if (mp[n] != -1) {
-            return mp[n];
+        if (dp[n] != -1) {
+            return dp[n];
         }
-          return mp[n] = fibonacci(n - 1, mp) + fibonacci(n - 2, mp);
+          return dp[n] = fibonacci(n - 1, dp) + fibonacci(n - 2, dp);
 
     
        
@@ -20,9 +20,9 @@ public class Dyna1d {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number : ");
         int n = sc.nextInt();
-        int[] mp = new int[n+1];
-        Arrays.fill(mp, -1);
-        System.out.println(n + "th fibonacci number is : " + fibonacci(n, mp));
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        System.out.println(n + "th fibonacci number is : " + fibonacci(n, dp));
 
     }
 
