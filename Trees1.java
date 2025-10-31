494de14dd6e91f43d5ae5f075a892a0e3cbf0282
Murli-Bhatt <murlibhatt52@gmail.com>
2025-10-31 08:54:43 +0530

    Add files via upload

---
+++ Trees1.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Trees1 {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+
+        }
+    }
+
+    public static class BuildTree {
+        int indx = -1;
+        // public BuildTrees(){
+
+        // }
+        public Node buildTree(int nodes[]) {
+            indx++;
+            if (nodes[indx] == -1) {
+                return null;                                 
+            }
+            Node nd = new Node(nodes[indx]);
+            nd.left = buildTree(nodes);
+            nd.right = buildTree(nodes);
+            return nd;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 8, 3, 10, -1, -1, -1, 14, -1, 12,-1,-1 };
+        BuildTree tree1 = new BuildTree();
+        Node root = tree1.buildTree(nodes);
+        System.out.println(root.data);
+
+    }
+
+}
