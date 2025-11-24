39aaf5786da81bffe783ff9d690a7052a5e24984
Murli-Bhatt <murlibhatt52@gmail.com>
2025-11-24 17:24:58 +0530

    Add files via upload

---
+++ PrintinRange.java
@@ -0,0 +1,65 @@
+
+//print the nodes in range of x,y
+import java.util.*;
+
+public class PrintinRange {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+
+        }
+    }
+
+    public static Node buildTree(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+
+            return root;
+
+        }
+
+        if (root.data > data) {
+            root.left = buildTree(root.left, data);
+        } else {
+            root.right = buildTree(root.right, data);
+        }
+        return root;
+
+    }
+
+    public static void printInRange(Node root, int x, int y) {
+        if (root == null) {
+            return;
+        }
+        if (x <= root.data && root.data <= y) {
+            printInRange(root.left, x, y);
+            System.out.print(root.data + " ");
+            printInRange(root.right, x, y);
+
+        }
+        if (root.data < x) {
+            printInRange(root.right, x, y);
+        } else if (root.data > y) {
+            printInRange(root.left, x, y);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] values = { 7, 3, 4, 2, 9, 8, 12, 1, 5 };
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = buildTree(root, values[i]);
+        }
+        System.out.println(root.data);
+        printInRange(root, 2, 5);
+
+    }
+
+}
